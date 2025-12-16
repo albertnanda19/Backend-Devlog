09cfd681d153961e2ff9c02aaa56b5a5ff38ca13
albertnanda19 <albertnanda1902@gmail.com>
2025-12-17 01:15:00 +0800

    feat: update new build file

--- dist/generated/prisma/browser.d.ts
+++
@@ -1,9 +0,0 @@
-import * as Prisma from './internal/prismaNamespaceBrowser.js';
-export { Prisma };
-export * as $Enums from './enums.js';
-export * from './enums.js';
-export type Role = Prisma.RoleModel;
-export type User = Prisma.UserModel;
-export type Project = Prisma.ProjectModel;
-export type Worklog = Prisma.WorklogModel;
-export type AuditLog = Prisma.AuditLogModel;
--- dist/generated/prisma/models/Worklog.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=Worklog.js.map
